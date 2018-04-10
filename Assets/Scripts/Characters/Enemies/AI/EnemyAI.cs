62ee1c0a449cc5bb09bb857427c5bea05b91e838
Maxime Cloutier-Gravel <maxcg.private@gmail.com>
2018-04-10 14:29:47 -0400

    Fixes
    - Made Ground Spike invulnerable.
    - Fixed the dimensions of the finish screen UI.

--- Assets/Scripts/Characters/Enemies/AI/EnemyAI.cs
+++ Assets/Scripts/Characters/Enemies/AI/EnemyAI.cs
@@ -16,6 +16,7 @@ public class EnemyAI : CharacterMotor
 
     public bool lookAtHero = false;
 	public bool isFlying = false;
+	public bool IsInvincible = false;
 
 	public bool stickToWalls = false;
 
@@ -62,12 +63,13 @@ public class EnemyAI : CharacterMotor
     }
 
 	public void TakeDamage() {
-		health--;
-        if (health < 1)
-        {
-            Kill();
-            _player.ResetDash();
-        }
+		if(!IsInvincible) {
+			health--;
+			if(health < 1) {
+				Kill();
+				_player.ResetDash();
+			}
+		}
 	}
     public void Kill()
     {
