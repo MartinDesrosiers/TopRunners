3fbaebe13c014922c56a774421fb5664eb2a6a8a
patrick patenaude <contrats@projettow2.net>
2018-04-14 01:14:32 -0400

    Multi language added

---
+++ Assets/Scripts/UI/UI_languageSelection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_languageSelection : MonoBehaviour 
+{
+	public string englishText = " ... english ...";
+	public string frenchText = " ... francais ...";
+
+	void OnEnable()
+	{				
+		MenuOptions.languageChanged += UpdateLanguage;
+		UpdateLanguage();
+	}
+
+	void OnDisable()
+	{			
+		MenuOptions.languageChanged -= UpdateLanguage;	
+	}
+
+	void UpdateLanguage()
+	{
+		
+		switch(MenuOptions.language)
+		{
+			case e_language.english:	this.gameObject.GetComponent<UnityEngine.UI.Text>().text = englishText;
+										break;
+			case e_language.french:		this.gameObject.GetComponent<UnityEngine.UI.Text>().text = frenchText;
+										break;	
+		}
+	}
+
+	public void SetText(string englishText = " ... ", string frenchText = " ... " )
+	{
+		this.englishText = englishText;
+		this.frenchText = frenchText;
+		UpdateLanguage();
+	}
+
+}
