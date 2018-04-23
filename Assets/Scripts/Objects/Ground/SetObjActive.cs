b210a8069dec372786e6854fb1551cc67b367348
Maxime Cloutier-Gravel <maxcg.private@gmail.com>
2018-04-23 02:34:44 -0400

    bug fix

--- Assets/Scripts/Objects/Ground/SetObjActive.cs
+++ Assets/Scripts/Objects/Ground/SetObjActive.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class SetObjActive : MonoBehaviour {
-	public void DestroyObject() {
-		Destroy(gameObject);
+
+	public IEnumerator SetObjectActive(float destrucTime) {
+		yield return new WaitForSeconds(destrucTime);
+		gameObject.GetComponent<Animator>().SetBool("Destroy", false);
+		gameObject.transform.localScale = new Vector2(1f, 1f);
+		gameObject.SetActive(false);
 	}
 }
